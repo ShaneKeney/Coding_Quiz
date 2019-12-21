a6e0beaf08f60160a159513c775a42ad4a9cf819
Shane Keney <shane@spikeball.com>
2019-12-21 14:18:31 -0500

    high score now being displayed correctly

--- scripts/highscores.js
+++ scripts/highscores.js
@@ -1,6 +1,9 @@
 var highScoreContainerEl = $("#highScoreContainer");
 var highScoreElement = $("#highScoreElement");
 
+var highScoreCon = document.querySelector("#highScoreContainer");
+var highScoreEl = document.querySelector("#highScoreElement");
+
 $("docuemnt").ready(() => {
     $('#goBack').on("click", goToMainPage);
     $('#clearScores').on("click", clearScores);
@@ -8,6 +11,8 @@ $("docuemnt").ready(() => {
 
     renderHighScores();
 });
+
+//renderHighScores();
 
 function goToMainPage() {
     window.location.href='./index.html';
@@ -24,11 +29,24 @@ function renderHighScores() {
     console.log(scoresArray);
     
     scoresArray.forEach((item, index) => {
-        let addHighScore = highScoreElement;
-        addHighScore.find("#highScoreText").text(`${index+1}. ${item.key} - ${item.value}`);
+        var highScoreDiv = document.createElement("div");
+        highScoreDiv.className = " d-flex pl-3 py-2 rounded-lg mt-2 test";
 
-        //not working for some reason
-        $("#highScoreContainer").append(addHighScore);
+        var highScoreText = document.createElement("p");
+        highScoreText.className = "mb-0";
+        highScoreText.id = "highScoreText";
+        highScoreText.textContent = `${index+1}. ${item.key} - ${item.value}`;
+
+        highScoreDiv.appendChild(highScoreText);
+        highScoreCon.appendChild(highScoreDiv);
+        
+        // let addHighScore = $(highScoreElement);
+        // addHighScore.find("#highScoreText").text(`${index+1}. ${item.key} - ${item.value}`);
+
+        // console.log(highScoreElement);
+
+        // //not working for some reason
+        // $("#highScoreContainer").append(addHighScore);
     });
 }
 
