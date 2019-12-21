6759362577101d0efd61eee1e1c3515cda862a15
Shane Keney <shane@spikeball.com>
2019-12-20 20:58:23 -0500

    end user prompt

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,7 @@ var startQuizEl = $("#quizDialog-container");
 var questionTemplate = $("#quizCardContainer");
 
 var TIMER_CONSTANT = questions.length * 15;
+var currentQuestionIndex = 0;
 
 $("docuemnt").ready(() => {
     $('#start-quiz').on("click", startTimer);
@@ -13,16 +14,15 @@ $("docuemnt").ready(() => {
 });
 
 function startTimer() {
-    var counter = TIMER_CONSTANT;
-    $("#timeIndicator").text(counter);
+    $("#timeIndicator").text(TIMER_CONSTANT);
 
     setInterval(function() {
-        counter --;
-        if(counter >=0) {
-            $("#timeIndicator").text(counter);
+        TIMER_CONSTANT --;
+        if(TIMER_CONSTANT >=0) {
+            $("#timeIndicator").text(TIMER_CONSTANT);
         }
-        if(counter === 0) {
-            clearInterval(counter);
+        if(TIMER_CONSTANT === 0) {
+            clearInterval(TIMER_CONSTANT);
             $("body").append(startQuizEl);
 
             //Rebind listeners for restarting quiz
@@ -32,12 +32,93 @@ function startTimer() {
     }, 1000);
 }
 
-function showQuestions() {
-    $("body").append(questionTemplate);
+function showAnswerStatus() {
+    let count = 2;
+
+    //displayed in multipleChoiceClick event function
+    //Hidden after timer expires
+
+    setInterval(function() {
+        count--;
+        if(count ===0) {
+            clearInterval(count);
+            $("#answerStatusContainer").addClass("d-none");
+        }
+    }, 1000)
+}
+
+function showNextQuestion(lastAnswerCorrect = undefined) {
+    //first remove old instance in order to remove prev attached event listeners
+    $('#quizCardContainer').remove();
+
+    //if there is no next question than cleanup and break;
+    if(currentQuestionIndex >= questions.length) {
+        $('#quizCardContainer').remove();
+
+        //TODO: render the ending prompt rather than another question
+
+
+        //Reset timer to original value
+        TIMER_CONSTANT = questions.length * 15;
+        currentQuestionIndex = 0;
+
+        return;
+    }
+
+    let newQuestion = createQuestionElement(currentQuestionIndex);
+
+    //show right or wrong
+    if(lastAnswerCorrect) {
+        newQuestion.find("#answerStatusContainer").remove("d-none"); 
+    }
+
+    $("body").append(newQuestion);
+}
+
+function createQuestionElement(questionIndex) {
+    console.log(`Creating Question ${questionIndex} element`);
+
+    let questionElement = questionTemplate;
+
+    //First create the question header
+    questionElement.find("#question-header").text(questions[questionIndex].title);
+
+    //Next render the button choices
+    let i=0;
+    questionElement.find("#button-container").find("button").each(function() {
+        $(this).html(`${questions[questionIndex].choices[i]}`);
+        
+        //Render button click event handler
+        $(this).on("click", multipleChoiceClick);
+        $(this).on("click", showAnswerStatus);
+
+        i++;
+    });
+
+    return questionElement;
+}
+
+function multipleChoiceClick(event) {
+    //show answer status
+    $("#answerStatusContainer").removeClass("d-none");
+
+    currentQuestionIndex++; //move to next question
+
+    let answerStatus;
+    //check to see if the answer is correct or incorrect and show prompt
+    if(event.currentTarget.innerHTML === questions[currentQuestionIndex - 1].answer) {
+        $("#answerStatus").text("CORRECT!")
+        answerStatus = true;
+    } else {
+        $("#answerStatus").text("WRONG!")
+        answerStatus = false;
+    }
+
+    showNextQuestion(answerStatus);
 }
 
 function startQuiz() {
     $("#quizDialog-container").remove();
 
-    showQuestions();
+    showNextQuestion();
 }
